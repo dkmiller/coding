4a171a1f7a43d6689092fe5469a0ebc59402c806
Daniel Miller <dm635@cornell.edu>
2016-07-21 15:03:40 -0400

    First commit

--- Crack/Program.cs
+++ Crack/Program.cs
@@ -10,6 +10,47 @@ namespace Crack
     {
         static void Main(string[] args)
         {
+            Console.Write("Input string: ");
+            var s = Console.ReadLine();
+            Console.WriteLine(HasUniqueCharacters(s));
+            Console.WriteLine(Reverse(s.ToCharArray()));
+        }
+
+        /// <summary>
+        /// Chapter 1, 1.1. Returns true if and only if the string s has unique 
+        /// characters.
+        /// </summary>
+        static bool HasUniqueCharacters(string s)
+        {
+            var count = new Dictionary<char, bool>();
+            foreach(char c in s) {
+                if (count.ContainsKey(c))
+                    return false;
+                else
+                    count[c] = true;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Chapter 1, 1.2. Reverses 
+        /// </summary>
+        static char[] Reverse(char[] s)
+        {
+            var l = s.Length;
+            char holder;
+
+            // String only needs to be reversed if more than one non-null char.
+            if (l > 2)
+            {
+                for (var i = 0; i < l/2; i++)
+                {
+                    holder = s[i];
+                    s[i] = s[l - 1 - i];
+                    s[l - 1 - i] = holder;
+                }
+            }
+            return s;
         }
     }
 }
