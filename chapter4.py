8ef573936b8e9bb8818cd9f3acd06814ede03082
Daniel Miller <dm635@cornell.edu>
2016-10-24 14:05:59 -0400

    sorted array to BST

--- chapter4.py
+++ chapter4.py
@@ -18,8 +18,8 @@ def isBalanced(root):
 				return False
 	return True
 
-# 4.2. Checks if there is a path between two nodes in a 
-# directed graph. Doesn't assume the graph is connected. 
+# 4.2. Checks if there is a path between two nodes in a
+# directed graph. Doesn't assume the graph is connected.
 def isPath(start, end):
 	done = set()
 	do = set([start])
@@ -33,3 +33,26 @@ def isPath(start, end):
 			if u not in done and u not in do:
 				do.add(u)
 	return False
+
+# 4.3. Given a sorted (increasing order) array, returns a
+# binary tree with minimal height.
+def array_to_tree(arr):
+	def att_bounds(arr, low, high):
+		if high - low == 0:
+			return Node(arr[low])
+		elif high - low == 1:
+			root = Node(arr[high])
+			root.left = Node(arr[low])
+			return root
+		elif high - low == 2:
+			root = Node(arr[low+1])
+			root.left = Node(arr[low])
+			root.right = Node(arr[high])
+			return root
+		else:
+			mid = (high-low+1)/2
+			root = Node(arr[mid])
+			root.left = att_bounds(arr,low,mid-1)
+			root.right = att_bounds(arr, mid+1,high)
+			return root
+	return att_bounds(arr, 0, len(arr)-1)
