6a29419c9290b7e42db8a26e9c6b63f651574fe3
Dan Miller <daniel.keegan.miller@gmail.com>
2021-12-03 09:07:26 -0800

    completed day3

--- advent_of_code/advent2021/tests/common.py
+++ advent_of_code/advent2021/tests/common.py
@@ -1,6 +1,6 @@
 import inspect
 from pathlib import Path
-from typing import Callable
+from typing import Callable, List
 
 
 def file_near(func: Callable, name: str) -> Path:
@@ -8,4 +8,18 @@ def file_near(func: Callable, name: str) -> Path:
     func_def_file = inspect.getabsfile(func)
     file_path = Path(func_def_file).parent / name
     rv = file_path.absolute()
+    return rv
+
+
+def read_file_near(func: Callable, name: str) -> str:
+    rv = file_near(func, name).read_text()
+    return rv
+
+
+def lines_file_near(func: Callable, name: str) -> List[str]:
+    """
+    List of lines in the file named `name` in the same directory as the file
+    defining `func`.
+    """
+    rv = read_file_near(func, name).splitlines()
     return rv
--- advent_of_code/advent2021/tests/test_day1.py
+++ advent_of_code/advent2021/tests/test_day1.py
@@ -1,19 +1,19 @@
 from advent_of_code.advent2021.day1 import number_of_increases
-from common import file_near
+from common import lines_file_near
 from pytest import mark
 
 
 @mark.parametrize("file,expected", [("report1.txt", 7), ("input.txt", 1709)])
 def test_number_of_increases(file, expected):
-    file_text = file_near(number_of_increases, file).read_text()
-    depths = [int(l) for l in file_text.splitlines()]
+    file_lines = lines_file_near(number_of_increases, file)
+    depths = [int(l) for l in file_lines]
     rv = number_of_increases(depths)
     assert rv == expected
 
 
 @mark.parametrize("file,expected", [("report1.txt", 5), ("input.txt", 1761)])
 def test_number_of_increases_window(file, expected):
-    file_text = file_near(number_of_increases, file).read_text()
-    depths = [int(l) for l in file_text.splitlines()]
+    file_lines = lines_file_near(number_of_increases, file)
+    depths = [int(l) for l in file_lines]
     rv = number_of_increases(depths, 3)
     assert rv == expected
